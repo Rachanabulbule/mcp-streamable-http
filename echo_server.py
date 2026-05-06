11fb12d374abbf870dc82869e19f69714151ebb1
Rachana Bulbule <rachana.bulbule@nice.com>
2026-05-06 18:07:08 +0530

    changed fastapi settings

--- echo_server.py
+++ echo_server.py
@@ -1,16 +1,16 @@
 from mcp.server.fastmcp import FastMCP
 from mcp.server.transport_security import TransportSecuritySettings
 
-mcp = FastMCP(name="EchoServer", stateless_http=True)
+# mcp = FastMCP(name="EchoServer", stateless_http=True)
 
-# mcp = FastMCP(
-#     "EchoServer",
-#     stateless_http=True,
-#     json_response=True,
-#     transport_security=TransportSecuritySettings(
-#         enable_dns_rebinding_protection=False  # for local dev / inspector
-#     )
-# )
+mcp = FastMCP(
+    "EchoServer",
+    stateless_http=True,
+    json_response=True,
+    transport_security=TransportSecuritySettings(
+        enable_dns_rebinding_protection=False  # for local dev / inspector
+    )
+)
 
 @mcp.tool(description="A simple echo tool")
 def echo(message: str) -> str:
--- math_server.py
+++ math_server.py
@@ -2,16 +2,16 @@ from mcp.server.fastmcp import FastMCP
 from mcp.server.transport_security import TransportSecuritySettings
 
 
-mcp = FastMCP(name="MathServer", stateless_http=True)
+# mcp = FastMCP(name="MathServer", stateless_http=True)
 
-# mcp = FastMCP(
-#     "MathServer",
-#     stateless_http=True,
-#     json_response=True,
-#     transport_security=TransportSecuritySettings(
-#         enable_dns_rebinding_protection=False
-#     )
-# )
+mcp = FastMCP(
+    "MathServer",
+    stateless_http=True,
+    json_response=True,
+    transport_security=TransportSecuritySettings(
+        enable_dns_rebinding_protection=False
+    )
+)
 
 @mcp.tool(description="Adds two to a number")
 def add_two(n: int) -> int:
