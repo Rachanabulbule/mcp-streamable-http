18831f1533f4bf12d3f4420275e41e9f9729a467
Rachana Bulbule <rachana.bulbule@nice.com>
2026-05-06 17:51:58 +0530

    initial code push

---
+++ math_server.py
@@ -0,0 +1,18 @@
+from mcp.server.fastmcp import FastMCP
+from mcp.server.transport_security import TransportSecuritySettings
+
+
+mcp = FastMCP(name="MathServer", stateless_http=True)
+
+# mcp = FastMCP(
+#     "MathServer",
+#     stateless_http=True,
+#     json_response=True,
+#     transport_security=TransportSecuritySettings(
+#         enable_dns_rebinding_protection=False
+#     )
+# )
+
+@mcp.tool(description="Adds two to a number")
+def add_two(n: int) -> int:
+    return n + 2
